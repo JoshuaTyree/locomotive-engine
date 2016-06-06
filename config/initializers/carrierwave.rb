9c2779f70f80dba8dce4a6c1ec3a6db8e4e25d39
Joshua Tyree <joshuat@createthebridge.com>
2016-06-06 01:33:44 -0500

    Updated

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,25 +2,30 @@ CarrierWave.configure do |config|
 
   config.cache_dir = File.join(Rails.root, 'tmp', 'uploads')
 
-  # config.storage = :file
-  # config.root = File.join(Rails.root, 'public')
+
   # WARNING: add the "carrierwave-aws" gem in your Rails app Gemfile.
   # More information here: https://github.com/sorentwo/carrierwave-aws
 
-  config.storage          = :aws
-  config.aws_bucket       = ENV['S3_BUCKET']
-  config.aws_acl          = 'public-read'
+  if ENV['S3_KEY_ID'].present?
+    config.storage          = :aws
+    config.aws_bucket       = ENV['S3_BUCKET']
+    config.aws_acl          = 'public-read'
 
-  config.aws_credentials  = {
-    access_key_id:      ENV['S3_KEY_ID'],
-    secret_access_key:  ENV['S3_SECRET_KEY'],
-    region:             ENV['S3_BUCKET_REGION']
-  }
+    config.aws_credentials  = {
+      access_key_id:      ENV['S3_KEY_ID'],
+      secret_access_key:  ENV['S3_SECRET_KEY'],
+      region:             ENV['S3_BUCKET_REGION']
+    }
 
-  # Put your CDN host below instead
-  if ENV['S3_ASSET_HOST_URL'].present?
-    config.asset_host = ENV['S3_ASSET_HOST_URL']
+    # Put your CDN host below instead
+    if ENV['S3_ASSET_HOST_URL'].present?
+      config.asset_host = ENV['S3_ASSET_HOST_URL']
+    end
+  else
+    config.storage = :file
+    config.root = File.join(Rails.root, 'public')
   end
+
 
   # case Rails.env.to_sym
   #
